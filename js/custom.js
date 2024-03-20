06362b10084b7171b7166f7776c34a4055df9cb1
nitish arali <nitisharali1995@gmail.com>
2024-03-20 15:46:22 +0530

    smtp

--- js/custom.js
+++ js/custom.js
@@ -1,128 +1,117 @@
 (function () {
+  "use strict";
 
-	'use strict'
+  AOS.init({
+    duration: 800,
+    easing: "slide",
+    once: true,
+  });
 
+  var preloader = function () {
+    var loader = document.querySelector(".loader");
+    var overlay = document.getElementById("overlayer");
 
-	AOS.init({
-		duration: 800,
-		easing: 'slide',
-		once: true
-	});
+    function fadeOut(el) {
+      el.style.opacity = 1;
+      (function fade() {
+        if ((el.style.opacity -= 0.1) < 0) {
+          el.style.display = "none";
+        } else {
+          requestAnimationFrame(fade);
+        }
+      })();
+    }
 
-	var preloader = function() {
+    setTimeout(function () {
+      fadeOut(loader);
+      fadeOut(overlay);
+    }, 200);
+  };
+  preloader();
 
-		var loader = document.querySelector('.loader');
-		var overlay = document.getElementById('overlayer');
+  var tinySdlier = function () {
+    var heroSlider = document.querySelectorAll(".hero-slide");
+    var propertySlider = document.querySelectorAll(".property-slider");
+    var imgPropertySlider = document.querySelectorAll(".img-property-slide");
+    var testimonialSlider = document.querySelectorAll(".testimonial-slider");
 
-		function fadeOut(el) {
-			el.style.opacity = 1;
-			(function fade() {
-				if ((el.style.opacity -= .1) < 0) {
-					el.style.display = "none";
-				} else {
-					requestAnimationFrame(fade);
-				}
-			})();
-		};
+    if (heroSlider.length > 0) {
+      var tnsHeroSlider = tns({
+        container: ".hero-slide",
+        mode: "carousel",
+        speed: 700,
+        autoplay: true,
+        controls: false,
+        nav: false,
+        autoplayButtonOutput: false,
+        controlsContainer: "#hero-nav",
+      });
+    }
 
-		setTimeout(function() {
-			fadeOut(loader);
-			fadeOut(overlay);
-		}, 200);
-	};
-	preloader();
-	
+    if (imgPropertySlider.length > 0) {
+      var tnsPropertyImageSlider = tns({
+        container: ".img-property-slide",
+        mode: "carousel",
+        speed: 700,
+        items: 1,
+        gutter: 30,
+        autoplay: true,
+        controls: false,
+        nav: true,
+        autoplayButtonOutput: false,
+      });
+    }
 
-	var tinySdlier = function() {
+    if (propertySlider.length > 0) {
+      var tnsSlider = tns({
+        container: ".property-slider",
+        mode: "carousel",
+        speed: 700,
+        gutter: 30,
+        items: 3,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        controlsContainer: "#property-nav",
+        responsive: {
+          0: {
+            items: 1,
+          },
+          700: {
+            items: 2,
+          },
+          900: {
+            items: 3,
+          },
+        },
+      });
+    }
 
-		var heroSlider = document.querySelectorAll('.hero-slide');
-		var propertySlider = document.querySelectorAll('.property-slider');
-		var imgPropertySlider = document.querySelectorAll('.img-property-slide');
-		var testimonialSlider = document.querySelectorAll('.testimonial-slider');
-		
-
-		if ( heroSlider.length > 0 ) {
-			var tnsHeroSlider = tns({
-				container: '.hero-slide',
-				mode: 'carousel',
-				speed: 700,
-				autoplay: true,
-				controls: false,
-				nav: false,
-				autoplayButtonOutput: false,
-				controlsContainer: '#hero-nav',
-			});
-		}
-
-
-		if ( imgPropertySlider.length > 0 ) {
-			var tnsPropertyImageSlider = tns({
-				container: '.img-property-slide',
-				mode: 'carousel',
-				speed: 700,
-				items: 1,
-				gutter: 30,
-				autoplay: true,
-				controls: false,
-				nav: true,
-				autoplayButtonOutput: false
-			});
-		}
-
-		if ( propertySlider.length> 0 ) {
-			var tnsSlider = tns({
-				container: '.property-slider',
-				mode: 'carousel',
-				speed: 700,
-				gutter: 30,
-				items: 3,
-				autoplay: true,
-				autoplayButtonOutput: false,
-				controlsContainer: '#property-nav',
-				responsive: {
-					0: {
-						items: 1
-					},
-					700: {
-						items: 2
-					},
-					900: {
-						items: 3
-					}
-				}
-			});
-		}
-
-
-		if ( testimonialSlider.length> 0 ) {
-			var tnsSlider = tns({
-				container: '.testimonial-slider',
-				mode: 'carousel',
-				speed: 700,
-				items: 3,
-				gutter: 50,
-				autoplay: true,
-				autoplayButtonOutput: false,
-				controlsContainer: '#testimonial-nav',
-				responsive: {
-					0: {
-						items: 1
-					},
-					700: {
-						items: 2
-					},
-					900: {
-						items: 3
-					}
-				}
-			});
-		}
-	}
-	tinySdlier();
-
-
-
-})()
+    if (testimonialSlider.length > 0) {
+      var tnsSlider = tns({
+        container: ".testimonial-slider",
+        mode: "carousel",
+        speed: 700,
+        items: 3,
+        gutter: 50,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        controlsContainer: "#testimonial-nav",
+        responsive: {
+          0: {
+            items: 1,
+          },
+          700: {
+            items: 2,
+          },
+          900: {
+            items: 3,
+          },
+        },
+      });
+    }
+  };
+  tinySdlier();
+})();
 
 // let mybutton = document.getElementById("btn-back-to-top");
 
@@ -148,3 +137,18 @@
 //   document.body.scrollTop = 0;
 //   document.documentElement.scrollTop = 0;
 // }
+
+// Email smtpjs
+function sendEmail(e) {
+  alert("clicked");
+  e.preventDefault();
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "website.chavadiconsultants@gmail.com",
+    Password: "45E70364F5721892E7D8315BA1A38011EC3D",
+    To: "nitisharali1995@gmail.com",
+    From: "website.chavadiconsultants@gmail.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
